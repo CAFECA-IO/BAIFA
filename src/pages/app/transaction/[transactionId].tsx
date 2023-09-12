6d9f1ed713ac2914fa8c859ae7ae9fce498cf958
Julian <julian890701@gmail.com>
2023-09-12 12:12:12 +0800

    transaction detail

--- src/pages/app/transaction/[transactionId].tsx
+++ src/pages/app/transaction/[transactionId].tsx
@@ -5,6 +5,7 @@ import NavBar from '../../../components/nav_bar/nav_bar';
 import TransactionDetail from '../../../components/transaction_detail/transaction_detail';
 import BoltButton from '../../../components/bolt_button/bolt_button';
 import Footer from '../../../components/footer/footer';
+import {BsArrowLeftShort} from 'react-icons/bs';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import {dummyTransactionData, ITransactionData} from '../../../interfaces/transaction_data';
 import {useTranslation} from 'next-i18next';
@@ -20,7 +21,7 @@ const TransactionDetailPage = ({transactionId, transactionData}: ITransactionDet
   const headTitle = `${t('TRANSACTION_DETAIL_PAGE.MAIN_TITLE')} ${transactionId} - BAIFA`;
 
   const router = useRouter();
-  const BackClickHandler = () => router.back();
+  const backClickHandler = () => router.back();
 
   return (
     <>
@@ -31,19 +32,29 @@ const TransactionDetailPage = ({transactionId, transactionData}: ITransactionDet
 
       <NavBar />
       <main>
-        <div className="flex min-h-screen flex-col overflow-hidden font-inter">
-          <div className="flex w-full flex-1 flex-col items-center space-y-10 px-20 pb-10 pt-28">
-            <div className="flex w-full items-center justify-center py-10 text-32px font-bold">
-              {t('TRANSACTION_DETAIL_PAGE.MAIN_TITLE')}{' '}
-              <span className="ml-2 text-primaryBlue">{transactionId}</span>
+        <div className="flex min-h-screen flex-col items-center overflow-hidden font-inter">
+          <div className="flex w-4/5 flex-1 flex-col items-center space-y-10 px-20 pb-10 pt-28">
+            <div className="flex w-full items-center justify-start py-10">
+              {/* Info: (20230912 -Julian) Back Arrow Button */}
+              <button onClick={backClickHandler}>
+                <BsArrowLeftShort className="text-48px" />
+              </button>
+              {/* Info: (20230912 -Julian) Transaction Title */}
+              <div className="flex flex-1 justify-center">
+                {/* ToDo: (20230912 -Julian) Blockchain icon */}
+                <h1 className="text-32px font-bold">
+                  {t('TRANSACTION_DETAIL_PAGE.MAIN_TITLE')}
+                  <span className="ml-2 text-primaryBlue"> {transactionId}</span>
+                </h1>
+              </div>
             </div>
 
             {/* Info: (20230907 - Julian) Transaction Detail */}
             <TransactionDetail transactionData={transactionData} />
 
-            <div className="">
+            <div className="pt-10">
               <BoltButton
-                onClick={BackClickHandler}
+                onClick={backClickHandler}
                 className="px-12 py-4 font-bold"
                 color="blue"
                 style="hollow"
