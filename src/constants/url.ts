42832c91321e6be842231a97feba535f42374bb5
Julian <julian890701@gmail.com>
2023-09-27 13:54:29 +0800

    add content

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,6 +4,7 @@ export const BFAURL = {
   CONTACT_US: '/contact-us',
   APP: '/app',
   CHAINS: '/app/chains',
+  CURRENCIES: '/app/currencies',
   TRACING_TOOL: '/app/tracing-tool',
   AUDITING_TOOL: '/app/auditing-tool',
   RED_FLAG: '/app/red-flag',
