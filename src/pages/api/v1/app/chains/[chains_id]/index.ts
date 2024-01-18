0b5ebac87c2f9b70e156bf636d768c9d34b36a20
Julian <julian890701@gmail.com>
2024-01-18 11:03:40 +0800

    fix 005

--- src/pages/api/v1/app/chains/[chains_id]/index.ts
+++ src/pages/api/v1/app/chains/[chains_id]/index.ts
@@ -1,32 +1,60 @@
 // 005 - GET /app/chains/:chains_id
 
 import type {NextApiRequest, NextApiResponse} from 'next';
-import pool from '../../../../../../lib/utils/dbConnection';
+import {getPrismaInstance} from '../../../../../../lib/utils/prismaUtils';
 
 type ResponseData = {
-  chainId: string;
+  chainId: number; // Info:(20240118 - Julian) 根據 DB 做調整，原本是 string;
   chainName: string;
   chainIcon: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  const prisma = getPrismaInstance();
+
   // Info: (20240112 - Julian) 解構 URL 參數，同時進行類型轉換
   const chains_id =
     typeof req.query.chains_id === 'string' ? parseInt(req.query.chains_id) : undefined;
 
-  pool.query(
-    `SELECT id as "chainId", 
-            chain_name as "chainName",
-            chain_icon as "chainIcon"
-     FROM chains
-     WHERE id = $1`,
-    [chains_id],
-    (err: Error, response: any) => {
-      if (!err) {
-        res.status(200).json(response.rows[0]);
+  const chainData = await prisma.chains.findUnique({
+    where: {
+      id: chains_id,
+    },
+    select: {
+      id: true,
+      chain_name: true,
+      chain_icon: true,
+    },
+  });
+
+  const result: ResponseData = chainData
+    ? {
+        chainId: chainData.id,
+        chainName: chainData.chain_name,
+        chainIcon: chainData.chain_icon,
       }
-    }
-  );
+    : // Info: (20240118 - Julian) 如果沒有找到資料，回傳空物件
+      {
+        chainId: 0,
+        chainName: '',
+        chainIcon: '',
+      };
+
+  res.status(200).json(result);
+
+  // pool.query(
+  //   `SELECT id as "chainId",
+  //           chain_name as "chainName",
+  //           chain_icon as "chainIcon"
+  //    FROM chains
+  //    WHERE id = $1`,
+  //   [chains_id],
+  //   (err: Error, response: any) => {
+  //     if (!err) {
+  //       res.status(200).json(response.rows[0]);
+  //     }
+  //   }
+  // );
 
   // pool.query(
   //   `SELECT id as "chainId",
