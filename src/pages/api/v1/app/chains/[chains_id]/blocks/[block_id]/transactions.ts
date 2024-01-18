29fa7372859ba3eb8bb5651aacc8e58b3dc2b40f
Julian <julian890701@gmail.com>
2024-01-18 16:00:17 +0800

    fix 016

--- src/pages/api/v1/app/chains/[chains_id]/blocks/[block_id]/transactions.ts
+++ src/pages/api/v1/app/chains/[chains_id]/blocks/[block_id]/transactions.ts
@@ -1,7 +1,7 @@
 // 008 - GET /app/chains/:chain_id/blocks/:block_id/transactions
 
 import type {NextApiRequest, NextApiResponse} from 'next';
-import pool from '../../../../../../../../lib/utils/dbConnection';
+import {getPrismaInstance} from '../../../../../../../../lib/utils/prismaUtils';
 
 type TransactionData = {
   id: string;
@@ -13,26 +13,36 @@ type TransactionData = {
 
 type ResponseData = TransactionData[];
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+  const prisma = getPrismaInstance();
   // Info: (20240116 - Julian) 解構 URL 參數，同時進行類型轉換
-  // const block_id = typeof req.query.block_id === 'string' ? req.query.block_id : undefined;
+  // const block_id =
+  //   typeof req.query.block_id === 'string' ? parseInt(req.query.block_id) : undefined;
 
-  pool.query(
-    `SELECT id,
-            chain_id as "chainId",
-            created_timestamp as "createdTimestamp",
-            type,
-            status
-      FROM transactions
-    WHERE block_hash = $1`,
-    ['0x424be71ddfe81a40cf6c005a546f4d2f09fce2fa05b9bcf5ca3eff3b6fc23738'],
-    // ToDo: (20240117 - Julian) 這邊先寫死，之後再補上 block_id
-    (err: Error, response: any) => {
-      if (!err) {
-        res.status(200).json(response.rows);
-      }
-    }
-  );
+  // Info: (20240116 - Julian) 從 DB 撈出所有位於 block_id 下的 transaction 的資料
+  const transactions = await prisma.transactions.findMany({
+    // ToDo: (20240118 - Julian) 要補上 block_id 的條件
+    select: {
+      id: true,
+      chain_id: true,
+      created_timestamp: true,
+      type: true,
+      status: true,
+    },
+  });
+
+  // Info: (20240118 - Julian) 轉換成 API 要的格式
+  const result: ResponseData = transactions.map(transaction => {
+    return {
+      id: `${transaction.id}`,
+      chainId: `${transaction.chain_id}`,
+      createdTimestamp: transaction.created_timestamp.getTime() / 1000,
+      type: transaction.type,
+      status: 'SUCCESS', // ToDo: (20240118 - Julian) 需要參考 codes Table 並補上 status 的轉換
+    };
+  });
+
+  res.status(200).json(result);
 
   /*   
   const result: ResponseData = [
--- src/pages/api/v1/app/chains/[chains_id]/index.ts
+++ src/pages/api/v1/app/chains/[chains_id]/index.ts
@@ -44,35 +44,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   res.status(200).json(result);
 
-  // pool.query(
-  //   `SELECT id as "chainId",
-  //           chain_name as "chainName",
-  //           chain_icon as "chainIcon"
-  //    FROM chains
-  //    WHERE id = $1`,
-  //   [chains_id],
-  //   (err: Error, response: any) => {
-  //     if (!err) {
-  //       res.status(200).json(response.rows[0]);
-  //     }
-  //   }
-  // );
-
-  // pool.query(
-  //   `SELECT id as "chainId",
-  //           chain_name as "chainName",
-  //           chain_icon as "chainIcon"
-  //    FROM chains
-  //     WHERE id = $1`,
-  //   [8017], // ToDo: (20240116 - Julian) 暫時先寫死，之後再補上 req.query.chains_id
-  //   (err: Error, response: any) => {
-  //     client.end();
-  //     if (!err) {
-  //       res.status(200).json(response.rows);
-  //     }
-  //   }
-  // );
-
   /*   const result: ResponseData = {
     'chainId': 'isun',
     'chainName': 'iSunCloud',
