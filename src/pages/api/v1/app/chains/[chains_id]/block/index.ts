b349f2e0c0e8df00bf9ecd514cc40d179018e72c
Julian <julian890701@gmail.com>
2024-01-25 17:17:57 +0800

    fix 007

--- src/pages/api/v1/app/chains/[chains_id]/block/index.ts
+++ src/pages/api/v1/app/chains/[chains_id]/block/index.ts
@@ -23,16 +23,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     typeof req.query.start_date === 'string' ? parseInt(req.query.start_date) : undefined;
   const end_date =
     typeof req.query.end_date === 'string' ? parseInt(req.query.end_date) : undefined;
+<<<<<<< HEAD:src/pages/api/v1/app/chains/[chains_id]/block/index.ts
   const page = typeof req.query.page === 'string' ? parseInt(req.query.page) : undefined;
 
   // Info: (20240119 - Julian) 計算分頁的 skip 與 take
   const skip = page ? (page - 1) * ITEM_PER_PAGE : undefined; // (20240119 - Julian) 跳過前面幾筆
   const take = ITEM_PER_PAGE; // (20240119 - Julian) 取幾筆
+=======
+  //const sort = typeof req.query.sort === 'string' ? req.query.sort : undefined;
+  //const page = typeof req.query.page === 'string' ? parseInt(req.query.page) : undefined;
+>>>>>>> 2492a5a (fix 007):src/pages/api/v1/app/chains/[chains_id]/blocks/index.ts
 
   // Info: (20240112 - Julian) 將 timestamp 轉換成 Date 物件
   const startDate = start_date ? new Date(start_date * 1000) : undefined;
   const endDate = end_date ? new Date(end_date * 1000) : undefined;
 
+<<<<<<< HEAD:src/pages/api/v1/app/chains/[chains_id]/block/index.ts
+=======
+  // Info: (20240112 - Julian) 將 sort 轉換成 SQL 的排序參數
+  // const sortArgument = sort ? (sort === 'SORTING.NEWEST' ? 'desc' : 'asc') : undefined;
+
+>>>>>>> 2492a5a (fix 007):src/pages/api/v1/app/chains/[chains_id]/blocks/index.ts
   const blocks = await prisma.blocks.findMany({
     where: {
       chain_id: chain_id,
