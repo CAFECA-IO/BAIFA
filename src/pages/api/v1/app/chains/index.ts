e0ea87d1f19ea886bdc6ced459ab21fdb3d04a07
Julian <julian890701@gmail.com>
2024-02-19 16:32:47 +0800

    fix api

--- src/pages/api/v1/app/chains/index.ts
+++ src/pages/api/v1/app/chains/index.ts
@@ -16,16 +16,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     },
   });
 
-  const blockCount = await prisma.blocks.count();
-  const transactionCount = await prisma.transactions.count();
+  // Info:(20240219 - Julian) 從 DB 撈出所有 block，再找出對應 chainId 的 block 數量
+  const allBLocks = await prisma.blocks.findMany({
+    select: {
+      chain_id: true,
+    },
+  });
+  const blockCount = chains.map(chain => {
+    return allBLocks.filter(block => block.chain_id === chain.id).length;
+  });
+
+  // Info:(20240219 - Julian) 從 DB 撈出所有 transaction，再找出對應 chainId 的 transaction 數量
+  const allTransactions = await prisma.transactions.findMany({
+    select: {
+      chain_id: true,
+    },
+  });
+  const transactionCount = chains.map(chain => {
+    return allTransactions.filter(transaction => transaction.chain_id === chain.id).length;
+  });
 
   // Info:(20240118 - Julian) 將撈出來的資料轉換成 API 要的格式
-  const result: ResponseData = chains.map(chain => {
+  const result: ResponseData = chains.map((chain, index) => {
     return {
       chainId: `${chain.id}`,
       chainName: `${chain.chain_name}`,
-      blocks: blockCount,
-      transactions: transactionCount,
+      blocks: blockCount[index],
+      transactions: transactionCount[index],
     };
   });
 
