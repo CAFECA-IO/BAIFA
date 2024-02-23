23a5981f48fe1375dea9c4ac1aa12089dfac379a
Liz <mingreal75@gmail.com>
2024-02-23 16:02:13 +0800

    fix red flag crash and refactor : instead array find of preprocess object

--- src/pages/api/v1/app/red_flags/[red_flag_id].ts
+++ src/pages/api/v1/app/red_flags/[red_flag_id].ts
@@ -2,9 +2,8 @@
 
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {getPrismaInstance} from '../../../../../lib/utils/prismaUtils';
-import {ITransaction} from '../../../../../interfaces/transaction';
 import {IRedFlagDetail} from '../../../../../interfaces/red_flag';
-import {AddressType, IAddressInfo} from '../../../../../interfaces/address_info';
+import {AddressType} from '../../../../../interfaces/address_info';
 
 type ResponseData = IRedFlagDetail | string;
 
@@ -16,7 +15,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   // Info: (20240131 - Liz) 若 URL 參數沒有成功轉換成數字，回傳 400
   if (Number.isNaN(red_flag_id)) {
-    res.status(400).send('400 - typeof red_flag_id must be Number');
+    res.status(400).send('400 - typeof red_flag_id cannot be transform into Number');
     return;
   }
 
@@ -28,6 +27,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       table_column: true,
     },
   });
+
+  // Info: (20240223 - Liz)  將 codes 預處理成物件以便有效率查找
+  const codeMap: {[key: string]: string | null} = {};
+  codes.forEach(code => {
+    const key = `${code.table_name}-${code.table_column}-${code.value}`;
+    codeMap[key] = code.meaning;
+  });
+
+  // Info: (20240223 - Liz)  定義一個幫助函數來查找 code 的意義
+  const findCodeMeaning = (tableName: string, tableColumn: string, value: string | null) => {
+    const key = `${tableName}-${tableColumn}-${value}`;
+    return codeMap[key] ?? 'Unknown Meaning';
+  };
 
   const redFlagData = await prisma.red_flags.findUnique({
     where: {
@@ -51,7 +63,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     return;
   }
 
-  // Info: (20240131 - Liz) 透過 redFlagData.related_transactions 從 transactions 表格讀取相關交易
+  // Info: (20240131 - Liz) 透過 redFlagData.related_transactions 從 transactions 表格讀取相關交易資料
   const transactions = await prisma.transactions.findMany({
     where: {
       hash: {
@@ -66,60 +78,47 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       to_address: true,
       status: true,
       type: true,
+      hash: true, // transaction hash
     },
   });
 
   // Info: (20240131 - Liz) 組合回傳資料
-  const id = redFlagData.id as unknown as string;
-  const chainId = redFlagData.chain_id as unknown as string;
-  const redFlagType = codes.find(
-    code =>
-      code.table_name === 'red_flags' &&
-      code.table_column === 'red_flag_type' &&
-      code.value === (redFlagData.red_flag_type ? parseInt(redFlagData.red_flag_type) : null)
-  )?.meaning as unknown as IRedFlagDetail['redFlagType'];
+  const id = `${redFlagData.id}`;
+  const chainId = `${redFlagData.chain_id}`;
+  const redFlagType = findCodeMeaning('red_flags', 'red_flag_type', redFlagData.red_flag_type);
 
-  const createdTimestamp = redFlagData.created_timestamp as unknown as number;
+  const createdTimestamp = redFlagData.created_timestamp ?? 0;
   const interactedAddresses = redFlagData.related_addresses.map(address => ({
-    id: address as unknown as string,
-    chainId: redFlagData.chain_id as unknown as string,
+    id: address ?? '',
+    chainId: `${redFlagData.chain_id}` ?? '',
   }));
 
-  const totalAmount = redFlagData.total_amount as unknown as number;
-  const unit = redFlagData.symbol as unknown as string;
+  const totalAmount = Number(redFlagData.total_amount) ?? 0; // ToDo: (20240223 - Liz) 想將 totalAmount 的 interface 型別改成 string
 
+  const unit = redFlagData.symbol ?? '';
+
+  // Info: (20240131 - Liz) 透過 transactions 資料組合 transactionHistoryData
   const transactionHistoryData = transactions.map(transaction => {
     const from = [
       {
-        address: transaction.from_address as unknown as string,
-        type: AddressType.ADDRESS as IAddressInfo['type'],
+        address: `${transaction.from_address}`,
+        type: AddressType.ADDRESS,
       },
     ];
     const to = [
       {
-        address: transaction.to_address as unknown as string,
-        type: AddressType.ADDRESS as IAddressInfo['type'],
+        address: `${transaction.to_address}`,
+        type: AddressType.ADDRESS,
       },
     ];
 
-    const status = codes.find(
-      code =>
-        code.table_name === 'transactions' &&
-        code.table_column === 'status' &&
-        code.value === (transaction.status !== null ? parseInt(transaction.status) : null)
-    )?.meaning as unknown as ITransaction['status'];
-
-    const type = codes.find(
-      code =>
-        code.table_name === 'transactions' &&
-        code.table_column === 'type' &&
-        code.value === (transaction.type ? parseInt(transaction.type) : null)
-    )?.meaning as unknown as ITransaction['type'];
+    const status = findCodeMeaning('transactions', 'status', transaction.status);
+    const type = findCodeMeaning('transactions', 'type', transaction.type);
 
     return {
-      id: transaction.id as unknown as string,
-      chainId: transaction.chain_id as unknown as string,
-      createdTimestamp: transaction.created_timestamp as unknown as number,
+      id: `${transaction.hash}`,
+      chainId: `${transaction.chain_id}`,
+      createdTimestamp: transaction.created_timestamp ?? 0,
       from,
       to,
       status,
@@ -139,52 +138,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   };
 
   res.status(200).json(result);
-
-  /*
-import {IRedFlagDetail} from '../../../../../interfaces/red_flag';
-
-type ResponseData = IRedFlagDetail | undefined;
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-  const result: ResponseData = {
-    'id': '140050038',
-    'chainId': 'usdt',
-    'redFlagType': 'RED_FLAG_DETAIL_PAGE.FLAG_TYPE_MULTIPLE_WITHDRAW',
-    'createdTimestamp': 1686579200,
-    'interactedAddresses': [
-      {
-        'id': '122134',
-        'chainId': 'eth',
-      },
-      {
-        'id': '134325',
-        'chainId': 'usdt',
-      },
-    ],
-    'totalAmount': 100,
-    'unit': 'USDT',
-    'transactionHistoryData': [
-      {
-        'id': '918402',
-        'chainId': 'btc',
-        'createdTimestamp': 1686579229,
-        'from': [{'type': 'address', 'address': '912299'}],
-        'to': [{'type': 'contract', 'address': '110132'}],
-        'status': 'SUCCESS',
-        'type': 'Crypto Currency',
-      },
-      {
-        'id': '912299',
-        'chainId': 'btc',
-        'createdTimestamp': 1687860718,
-        'from': [{'type': 'address', 'address': '110132'}],
-        'to': [{'type': 'contract', 'address': '310683'}],
-        'status': 'PENDING',
-        'type': 'Crypto Currency',
-      },
-      //...
-    ],
-  };
-  res.status(200).json(result);
-  */
 }
