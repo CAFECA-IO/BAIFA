aabc2e387996e3e7c344bd0eaec24bf4353d985c
Liz <mingreal75@gmail.com>
2024-02-26 17:39:52 +0800

    add tooltip contents : currencies detail page

--- src/pages/api/v1/app/currencies/[currency_id]/index.ts
+++ src/pages/api/v1/app/currencies/[currency_id]/index.ts
@@ -222,7 +222,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     }
   });
 
-  // Info: (今天 - Liz) 將 transactionData 轉換成 transactionHistoryData
+  // Info: (20240226 - Liz) 將 transactionData 轉換成 transactionHistoryData
   const transactionHistoryData: ITransaction[] = transactionData.map(transaction => {
     // Info: (20240130 - Julian) from address 轉換
     const fromAddresses = transaction.from_address ? transaction.from_address.split(',') : [];
