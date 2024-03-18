d35b3e37a77f5b122474adcb8e499c92a69aa14f
Liz <mingreal75@gmail.com>
2024-03-18 18:48:33 +0800

    Remove holders with negative holding amounts from the top 100 holders

--- src/pages/api/v1/app/currencies/[currency_id]/top100Holders.ts
+++ src/pages/api/v1/app/currencies/[currency_id]/top100Holders.ts
@@ -62,6 +62,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       const holderData100Top = await prisma.token_balances.findMany({
         where: {
           currency_id: currency_id,
+          NOT: {
+            value: {
+              startsWith: '-', // Info: (20240318 - Liz) 不包含負數
+            },
+          },
         },
         select: {
           address: true,
@@ -84,6 +89,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       const holderData100TopPaged = await prisma.token_balances.findMany({
         where: {
           currency_id: currency_id,
+          NOT: {
+            value: {
+              startsWith: '-', // Info: (20240318 - Liz) 不包含負數
+            },
+          },
         },
         select: {
           address: true,
@@ -117,35 +127,41 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         : totalPagesOfAllHolders;
 
     const holderDataFormat: IHolder[] = holderData.map(holder => {
-      // Info: (20240220 - Liz) 取得持有價值(資料庫欄位值設定為64位數字串前方補零)並將負數直接歸零
-      const holdingValue = holder.value && !holder.value.startsWith('-') ? holder.value : '0';
+      // Info: (20240220 - Liz) 取得持有價值 (資料庫欄位值設定為64位數字串前方補零)
+      const holdingValue = holder.value ?? '0';
 
-      // Info: (20240220 - Liz) 持有數量格式化(把價值轉換成數量): 切成兩部分，小數部分有(decimal) 18 位數、整數部分加上千分位逗號
+      // Info: (20240220 - Liz) 持有數量格式化(把價值轉換成數量): 切成兩部分，小數部分有(decimal)位數、整數部分另做處理
       const splitIndex = holdingValue.length - decimal;
       const firstPart = holdingValue.substring(0, splitIndex);
       const secondPart = holdingValue.substring(splitIndex);
-      // Info: (20240313 - Liz) 去除整數部分前面的所有 '0'(判斷整數部分是否為空字串，是就補一個'0')，每三位數加上千分位逗號
+
+      // Info: (20240313 - Liz) 去除整數部分前面的所有'0'(判斷整數部分是否為空字串，是就補一個'0')，每三位數加上千分位逗號
       const firstPartFormat = firstPart
         .replace(/^0*/, match => (match === '' ? '0' : ''))
         .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
       const holdingAmount = `${firstPartFormat}.${secondPart}`;
 
-      // Info: (20240220 - Liz) 計算持有比例
-      const holdingValueBigInt = BigInt(holdingValue);
-      const scale = BigInt(10000);
-      const scaleHoldingValueBigInt = holdingValueBigInt * scale;
-      const holdingPercentageBigInt = scaleHoldingValueBigInt / BigInt(totalAmountOfCurrency);
-      const holdingPercentageString = holdingPercentageBigInt.toString();
+      // Info: (20240318 - Liz) 計算持有比例
+      function calculateHoldingPercentage(holdingValue: string, totalAmountOfCurrency: string) {
+        const holdingValueBigInt = BigInt(holdingValue);
+        const scale = BigInt(10000);
+        const scaleHoldingValueBigInt = holdingValueBigInt * scale;
+        const holdingPercentageBigInt = scaleHoldingValueBigInt / BigInt(totalAmountOfCurrency);
+        const holdingPercentageString = holdingPercentageBigInt.toString();
 
-      // Info: (20240220 - Liz) 將持有比例格式化為小數點後2位小數(原本scale 10000倍，這裡只有縮小100倍是因為要直接換算成百分比)
-      const formatString = (str: string) => {
-        const paddedA = str.padStart(3, '0'); // Info: (20240220 - Liz) 字串前面補零，直到長度為3，原長度大於或等於3時不會補零
-        const integerPart = paddedA.slice(0, -2); // Info: (20240220 - Liz) 整數部分
-        const decimalPart = paddedA.slice(-2); // Info: (20240220 - Liz) 小數部分
-        return `${integerPart}.${decimalPart}`;
-      };
-      const holdingPercentage = formatString(holdingPercentageString);
+        // Info: (20240220 - Liz) 將持有比例格式化為小數點後2位小數(原本scale 10000倍，這裡只有縮小100倍是因為要直接換算成百分比)
+        const formatString = (str: string) => {
+          const paddedA = str.padStart(3, '0'); // Info: (20240220 - Liz) 字串前面補零，直到長度為3，原長度大於或等於3時不會補零
+          const integerPart = paddedA.slice(0, -2); // Info: (20240220 - Liz) 整數部分
+          const decimalPart = paddedA.slice(-2); // Info: (20240220 - Liz) 小數部分
+          return `${integerPart}.${decimalPart}`;
+        };
+
+        return formatString(holdingPercentageString);
+      }
+
+      const holdingPercentage = calculateHoldingPercentage(holdingValue, totalAmountOfCurrency);
 
       // Info: (20240202 - Julian) 計算持有比例的 bar 寬度
       const holdingBarWidth = Number(holdingPercentage);
