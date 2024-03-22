16359d1877e5a121a9ba179198ace12db8777709
Liz <mingreal75@gmail.com>
2024-03-22 16:17:34 +0800

    currencies list page: call API by useAPIResponse

--- src/components/all_currencies_page_body/all_currencies_page_body.tsx
+++ src/components/all_currencies_page_body/all_currencies_page_body.tsx
@@ -1,5 +1,4 @@
 import {useState, useEffect} from 'react';
-//import {MarketContext} from '../../contexts/market_context';
 import Footer from '../footer/footer';
 import Breadcrumb from '../breadcrumb/breadcrumb';
 import CurrencyItem from '../currency_item/currency_item';
@@ -18,38 +17,37 @@ import {getKeyByValue} from '../../lib/common';
 
 const AllCurrenciesPageBody = () => {
   const {t}: {t: TranslateFunction} = useTranslation('common');
-  //const {getCurrencies} = useContext(MarketContext);
 
-  const sortingOptions = ['A to Z', 'Z to A']; // Info: (20240125 - Julian) 暫時以字母排序
-  const typeOptionDefault = 'SORTING.ALL';
-
-  const [filteredType, setFilteredType] = useState(typeOptionDefault);
   // Info: (20240308 - Liz) 搜尋條件
   const [search, setSearch] = useState('');
-  // Info: (20240308 - Liz) 如果是選擇 A ot Z, API req 參數 sort=asc, 如果是選擇 Z to A, API 參數 sort=desc
-  const [sorting, setSorting] = useState(sortingOptions[0]);
-  // Info: (20240308 - Liz) UI
-  //const [currenciesData, setCurrenciesData] = useState<ICurrencyListPage>();
+  const typeOptionDefault = 'SORTING.ALL';
+  const [filteredType, setFilteredType] = useState(typeOptionDefault);
   const [activePage, setActivePage] = useState<number>(1);
+
+  const sortingOptions = ['A to Z', 'Z to A']; // Info: (20240125 - Julian) 暫時以字母排序
+  const [sorting, setSorting] = useState(sortingOptions[0]);
 
   const [filteredTypeByChainId, setFilteredTypeByChainId] = useState('');
 
+  // Info: (20240308 - Liz) 如果是選擇 A ot Z, API req 參數 sort=asc, 如果是選擇 Z to A, API 參數 sort=desc
   const sortingMap = {
     [sortingOptions[0]]: 'asc',
     [sortingOptions[1]]: 'desc',
   };
   const sortingReq = sortingMap[sorting];
 
-  const {data: currenciesData, isLoading} = useAPIResponse<ICurrencyListPage>(
-    `${APIURL.CURRENCIES}`,
-    {method: HttpMethod.GET},
-    {
-      page: activePage,
-      sort: sortingReq,
-      search: search,
-      type: filteredTypeByChainId,
-    }
-  );
+  const {data: currenciesData, isLoading: isCurrenciesDataLoading} =
+    useAPIResponse<ICurrencyListPage>(
+      `${APIURL.CURRENCIES}`,
+      {method: HttpMethod.GET},
+      // Info: (今天 - Liz) 預設值 ?page=1&sort=asc&search=&type=
+      {
+        page: activePage,
+        sort: sortingReq,
+        search: search,
+        type: filteredTypeByChainId,
+      }
+    );
 
   // Info: (20240319 - Liz) chain id 和 chain name 的對應物件
   useEffect(() => {
@@ -62,45 +60,14 @@ const AllCurrenciesPageBody = () => {
   // Info: (20240308 - Liz) 從 API 取得總頁數
   const totalPages = currenciesData?.totalPages ?? 0;
 
-  // Info: (20240308 - Liz) API 查詢參數
-  //const [apiQueryStr, setApiQueryStr] = useState(`page=1&sort=asc&search=&type=`);
-
   // Info: (20240308 - Liz) 下拉式選單選項(chain id)由 API 回傳
   const chainNameTypes = currenciesData?.chainNameTypes ?? [];
   const typeOptions = [typeOptionDefault, ...chainNameTypes];
 
   // Info: (20240308 - Liz) 當搜尋、篩選、排序的條件改變時，將 activePage 設為 1。
-  /*   useEffect(() => {
+  useEffect(() => {
     setActivePage(1);
   }, [search, filteredType, sorting]);
-
-  // Info: (20240308 - Liz) Call API to get currencies data
-  useEffect(() => {
-    const fetchCurrencies = async () => {
-      try {
-        const data = await getCurrencies(apiQueryStr);
-        setCurrenciesData(data);
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error('getCurrencies error', error);
-      }
-    };
-
-    fetchCurrencies();
-    // Info: (20240308 - Liz) 當 API 查詢參數改變時，重新取得資料
-  }, [apiQueryStr, getCurrencies]); */
-
-  // Info: (20240305 - Liz) 設定 API 查詢參數
-  /*   useEffect(() => {
-    const pageQuery = `page=${activePage}`;
-    const sortQuery = `&sort=${sortingReq}`;
-    const searchQuery = `&search=${search}`;
-
-    const typeQuery =
-      filteredType === typeOptionDefault ? `&type=${''}` : `&type=${filteredTypeByChainId}`;
-
-    setApiQueryStr(`${pageQuery}${sortQuery}${searchQuery}${typeQuery}`);
-  }, [activePage, filteredType, filteredTypeByChainId, search, sortingReq]); */
 
   // Info: (20240308 - Liz) 顯示貨幣列表
   const currenciesList =
@@ -136,7 +103,7 @@ const AllCurrenciesPageBody = () => {
     </div>
   );
 
-  const displayCurrenciesList = isLoading ? skeletonCurrenciesList : currenciesList;
+  const displayCurrenciesList = isCurrenciesDataLoading ? skeletonCurrenciesList : currenciesList;
 
   // Info: (20240308 - Liz) Breadcrumb
   const crumbs = [
