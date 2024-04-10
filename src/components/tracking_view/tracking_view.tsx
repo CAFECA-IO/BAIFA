6b507e8e01217ff05fde3d8b749027b383009c57
Julian <julian890701@gmail.com>
2024-04-10 18:50:14 +0800

    feat: second layer graph

--- src/components/tracking_view/tracking_view.tsx
+++ src/components/tracking_view/tracking_view.tsx
@@ -1,9 +1,21 @@
-import {useContext, useEffect, useRef} from 'react';
+import {useContext, useEffect, useRef, useState} from 'react';
 import * as d3 from 'd3';
 import {TrackingContext} from '../../contexts/tracking_context';
-import useAPIResponse from '../../lib/hooks/use_api_response';
-import {HttpMethod} from '../../constants/api_request';
 import {truncateText} from '../../lib/common';
+
+interface INode {
+  id: string;
+  group: string;
+}
+interface ILink {
+  source: string;
+  target: string;
+}
+
+interface IGraphData {
+  nodes: INode[];
+  links: ILink[];
+}
 
 const TrackingView = () => {
   const {
@@ -18,81 +30,159 @@ const TrackingView = () => {
   const blockchainQueryStr = filterBlockchains.length > 0 ? filterBlockchains.join(',') : '';
   const currencyQueryStr = filterCurrencies.length > 0 ? filterCurrencies.join(',') : '';
 
-  const {data: interactionAddresses, isLoading} = useAPIResponse<string[]>(
-    `/api/v1/app/tracking_tool/interaction_list`,
-    {method: HttpMethod.GET},
-    {
-      address_id: targetAddress,
-      blockchains: blockchainQueryStr,
-      currencies: currencyQueryStr,
-      start_date: filterDatePeriod.startTimeStamp === 0 ? '' : filterDatePeriod.startTimeStamp,
-      end_date: filterDatePeriod.endTimeStamp === 0 ? '' : filterDatePeriod.endTimeStamp,
+  const [graphData, setGraphData] = useState<IGraphData>({
+    nodes: [],
+    links: [],
+  });
+
+  const [isOpenFirstLayer, setIsOpenFirstLayer] = useState<boolean>(false);
+  const [firstLayerInteractions, setFirstLayerInteractions] = useState<string[]>([]);
+
+  const [isOpenSecondLayer, setIsOpenSecondLayer] = useState<boolean>(false);
+  const [secondLayerInteractions, setSecondLayerInteractions] = useState<string[]>([]);
+
+  const [firstLayerSelectedAddress, setFirstLayerSelectedAddress] = useState<string>('');
+
+  const openFirstLayerHandler = () => setIsOpenFirstLayer(!isOpenFirstLayer);
+  const openSecondLayerHandler = () => setIsOpenSecondLayer(!isOpenSecondLayer);
+
+  const getInteractions = async (address: string) => {
+    let interactions;
+    try {
+      const response = await fetch(
+        `/api/v1/app/tracking_tool/interaction_list?address_id=${address}&blockchains=${blockchainQueryStr}&currencies=${currencyQueryStr}&start_date=${
+          filterDatePeriod.startTimeStamp === 0 ? '' : filterDatePeriod.startTimeStamp
+        }&end_date=${filterDatePeriod.endTimeStamp === 0 ? '' : filterDatePeriod.endTimeStamp}
+      `,
+        {
+          method: 'GET',
+        }
+      );
+      interactions = await response.json();
+    } catch (error) {
+      console.log(error);
     }
-  );
-
-  const interactions = interactionAddresses ?? [];
-
-  interface INode {
-    id: string;
-    group: string;
-  }
-  interface ILink {
-    source: string;
-    target: string;
-  }
-
-  // Info: (20240327 - Julian) graph data
-  const data: {
-    nodes: INode[];
-    links: ILink[];
-  } = {
-    nodes: [
-      {id: targetAddress, group: 'target'},
-      ...interactions.map(address => {
-        return {id: address, group: 'interactions'};
-      }),
-    ],
-    links: [
-      {source: targetAddress, target: targetAddress},
-      ...interactions.map(address => {
-        return {source: address, target: targetAddress};
-      }),
-    ],
+    return interactions;
   };
+
+  useEffect(() => {
+    // Info: (20240410 - Julian) 如果 targetAddress 改變，則關閉第一層
+    setIsOpenFirstLayer(false);
+  }, [targetAddress]);
+
+  useEffect(() => {
+    if (isOpenFirstLayer) {
+      getInteractions(targetAddress).then(interactions => {
+        setFirstLayerInteractions(interactions);
+      });
+    } else {
+      setFirstLayerInteractions([]);
+    }
+
+    if (isOpenSecondLayer) {
+      getInteractions(firstLayerSelectedAddress).then(interactions => {
+        setSecondLayerInteractions(interactions);
+      });
+    } else {
+      setSecondLayerInteractions([]);
+    }
+  }, [filterBlockchains, filterCurrencies, filterDatePeriod, isOpenFirstLayer, isOpenSecondLayer]);
+
+  useEffect(() => {
+    // Info: (20240410 - Julian) graph data
+    const graph = {
+      nodes: [
+        // {id: '0000', group: 'target'},
+        // {id: '1001', group: 'firstLayerInteractions'},
+        // {id: '1002', group: 'firstLayerInteractions'},
+        // {id: '1003', group: 'firstLayerInteractions'},
+        // {id: '2009', group: 'secondLayerInteractions'},
+        {id: targetAddress, group: 'target'},
+        ...firstLayerInteractions.map(address => {
+          return {id: address, group: 'firstLayerInteractions'};
+        }),
+        ...secondLayerInteractions
+          // Info: (20240410 - Julian) 過濾掉 targetAddress
+          .filter(address => address !== targetAddress)
+          .map(address => {
+            return {id: address, group: 'secondLayerInteractions'};
+          }),
+      ],
+      links: [
+        // {source: '0000', target: '1001'},
+        // {source: '0000', target: '1002'},
+        // {source: '0000', target: '1003'},
+        // {source: '1001', target: '2009'},
+        {source: targetAddress, target: targetAddress},
+        ...firstLayerInteractions.map(address => {
+          return {source: address, target: targetAddress};
+        }),
+        ...secondLayerInteractions
+          // Info: (20240410 - Julian) 過濾掉 targetAddress
+          .filter(address => address !== targetAddress)
+          .map(address => {
+            return {source: address, target: firstLayerSelectedAddress};
+          }),
+      ],
+    };
+
+    setGraphData(graph);
+  }, [firstLayerInteractions, secondLayerInteractions]);
 
   const mapRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const links = data.links.map(Object.create);
-    const nodes = data.nodes.map(Object.create);
+    const links = graphData.links.map(Object.create);
+    const nodes = graphData.nodes.map(Object.create);
 
     // Info: (20240327 - Julian) 圖表的直徑
     const DIAMETER_OF_MAP = 200;
+    // Info: (20240410 - Julian) 第一層和第二層節點的數量
+    const firstLayerLength = nodes.filter(node => node.group === 'firstLayerInteractions').length;
+    const secondLayerLength = nodes.filter(node => node.group === 'secondLayerInteractions').length;
 
-    // Info: (20240329 - Julian) 點的半徑會隨著點的數量變動
-    const nodeRadius = 500 / nodes.length > 64 ? 64 : 500 / nodes.length; // Info: (20240329 - Julian) 點的半徑最大為 64
-    const strokeWidth = 100 / nodes.length > 10 ? 10 : 100 / nodes.length; // Info: (20240329 - Julian) 輪廓的寬度最大為 10
+    // Info: (20240410 - Julian) firstSelectedAddress 的 X 座標
+    const firstAddressX =
+      DIAMETER_OF_MAP *
+      Math.cos(
+        ((nodes.indexOf(nodes.find(node => node.id === firstLayerSelectedAddress)) - 1) *
+          (2 * Math.PI)) /
+          firstLayerLength
+      );
+    // Info: (20240410 - Julian) firstSelectedAddress 的 Y 座標
+    const firstAddressY =
+      DIAMETER_OF_MAP *
+      Math.sin(
+        ((nodes.indexOf(nodes.find(node => node.id === firstLayerSelectedAddress)) - 1) *
+          (2 * Math.PI)) /
+          firstLayerLength
+      );
 
-    // Info: (20240329 - Julian) 計算圓心的 x 座標
+    // Info: (20240329 - Julian) 計算節點圓心的 x 座標
     const getCircleCenterX = (i: number) => {
-      // i = 0 代表 target address，也就是圓心，所以直接回傳 0
-      // 其他的 i 代表 interactions，以 360 度平均分配，順時針排列
+      // Info: (20240410 - Julian) i = 0 代表 target address，也就是圓心，所以直接回傳 0
       return i === 0
         ? 0
-        : DIAMETER_OF_MAP * Math.cos(((i - 1) * (2 * Math.PI)) / (nodes.length - 1));
+        : // Info: (20240410 - Julian) 如果 i > firstLayerLength，代表是 secondLayerInteractions
+        // 以 125為半徑，偏移量 = firstSelectedAddress 的 x 座標
+        i > firstLayerLength
+        ? 125 * Math.cos(((i - 1) * (2 * Math.PI)) / secondLayerLength) + firstAddressX
+        : // Info: (20240410 - Julian) 其他的 i 代表 firstLayerInteractions，以 360 度平均分配，順時針排列
+          DIAMETER_OF_MAP * Math.cos(((i - 1) * (2 * Math.PI)) / firstLayerLength);
     };
 
-    // Info: (20240329 - Julian) 計算圓心的 y 座標
+    // Info: (20240329 - Julian) 計算節點圓心的 y 座標
     const getCircleCenterY = (i: number) => {
-      // i = 0 代表 target address，也就是圓心，所以直接回傳 0
-      // 其他的 i 代表 interactions，以 360 度平均分配，順時針排列
+      // Info: (20240410 - Julian) i = 0 代表 target address，也就是圓心，所以直接回傳 0
       return i === 0
         ? 0
-        : DIAMETER_OF_MAP * Math.sin(((i - 1) * (2 * Math.PI)) / (nodes.length - 1));
+        : // Info: (20240410 - Julian) 如果 i > firstLayerLength，代表是 secondLayerInteractions
+        // 以 125為半徑，偏移量 = firstSelectedAddress 的 y 座標
+        i > firstLayerLength
+        ? 125 * Math.sin(((i - 1) * (2 * Math.PI)) / secondLayerLength) + firstAddressY
+        : // Info: (20240410 - Julian) 其他的 i 代表 firstLayerInteractions，以 360 度平均分配，順時針排列
+          DIAMETER_OF_MAP * Math.sin(((i - 1) * (2 * Math.PI)) / firstLayerLength);
     };
-
-    // Info: (20240329 - Julian) 點之間的引力
-    const forceStrength = nodes.length * -1000;
 
     if (!mapRef.current) return;
 
@@ -148,6 +238,25 @@ const TrackingView = () => {
       .style('stop-color', '#F89B29') // Info: (20240328 - Julian) 結束色
       .style('stop-opacity', 1);
 
+    const linear3 = svg
+      .append('defs')
+      .append('linearGradient')
+      .attr('id', 'blueGreenLinear') // Info: (20240410 - Julian) 藍綠漸變色
+      .attr('x1', '0%')
+      .attr('x2', '0%')
+      .attr('y1', '0%')
+      .attr('y2', '100%'); // Info: (20240410 - Julian) 0% 100% 0% 0% 代表從上到下漸變
+    linear3
+      .append('stop')
+      .attr('offset', '0%')
+      .style('stop-color', '#00C7FF')
+      .style('stop-opacity', 1); // Info: (20240410 - Julian) 起始色
+    linear3
+      .append('stop')
+      .attr('offset', '100%')
+      .style('stop-color', '#00FFA3') // Info: (20240410 - Julian) 結束色
+      .style('stop-opacity', 1);
+
     // Info: (20240328 - Julian) 連線
     const link = svg
       .append('g')
@@ -164,24 +273,37 @@ const TrackingView = () => {
       .selectAll('circle')
       .data(nodes)
       .join('circle')
-      .attr('class', d => (d.group === 'target' ? 'target' : 'interactions')) // Info: (20240329 - Julian) 賦予 target address 和 interactions 不同的 class
-      .attr('stroke-width', strokeWidth)
-      .attr('r', nodeRadius)
+      .attr('class', d => d.group) // Info: (20240329 - Julian) 賦予各個點不同的 class
+      .attr('stroke-width', 5)
+      .attr('r', 40)
       .attr('fill', function (d) {
         // Info: (20240403 - Julian) 如果沒有被選到，則顯示黑色
         if (d.id !== selectedItems[0] && d.id !== selectedItems[1]) return '#161830';
         // Info: (20240403 - Julian) 根據 class 來決定顏色
         return d3.select(this).classed('target')
           ? 'url(#bluePurpleLinear)'
-          : 'url(#redOrangeLinear)';
+          : d3.select(this).classed('firstLayerInteractions')
+          ? 'url(#redOrangeLinear)'
+          : 'url(#blueGreenLinear)';
       })
       .style('stroke', function () {
         // Info: (20240329 - Julian) 根據 class 來決定顏色
         return d3.select(this).classed('target')
           ? 'url(#bluePurpleLinear)'
-          : 'url(#redOrangeLinear)';
+          : d3.select(this).classed('firstLayerInteractions')
+          ? 'url(#redOrangeLinear)'
+          : 'url(#blueGreenLinear)';
       })
-      // Info: (20240403 - Julian) 點擊時觸發事件
+      // Info: (20240410 - Julian) 雙擊時，展開該點的資訊
+      .on('dblclick', function (_, d) {
+        if (d.group === 'target') {
+          openFirstLayerHandler();
+        } else if (d.group === 'firstLayerInteractions') {
+          setFirstLayerSelectedAddress(d.id);
+          openSecondLayerHandler();
+        }
+      })
+      // Info: (20240403 - Julian) 點擊時，將 id 寫入 selectedItems
       .on('click', function (_, d) {
         selectedItemsHandler(d.id);
       })
@@ -215,40 +337,58 @@ const TrackingView = () => {
       .attr('fill', '#F0F0F0')
       .text(d => truncateText(d.id, 7));
 
+    const getStartX = (i: number) => {
+      // 第二層的起點為 firstSelectedAddress 的圓心
+      return i > firstLayerLength
+        ? getCircleCenterX(firstLayerInteractions.indexOf(firstLayerSelectedAddress) + 1)
+        : 0;
+    };
+    const getStartY = (i: number) => {
+      // 第二層的起點為 firstSelectedAddress 的圓心
+      return i > firstLayerLength
+        ? getCircleCenterY(firstLayerInteractions.indexOf(firstLayerSelectedAddress) + 1)
+        : 0;
+    };
+
     const simulation = d3
       .forceSimulation(nodes)
       // Info: (20240329 - Julian) 連結的引力
       .force(
         'link',
-        d3.forceLink(data.links).id((d: any) => d.id)
+        d3.forceLink(graphData.links).id((d: any) => d.id)
       )
       // Info: (20240329 - Julian) alphaTarget 代表模擬的 alpha 值，alpha 值越高，模擬的速度越快
-      .alphaTarget(0.3)
+      .alphaTarget(0.1)
       // Info: (20240329 - Julian) 點之間的引力
-      .force('charge', d3.forceManyBody().strength(forceStrength))
+      .force('charge', d3.forceManyBody().strength(-1600))
       .force('x', d3.forceX())
       .force('y', d3.forceY())
       .on('tick', () => {
         link
-          .attr('x1', () => 0) // x1 起點 = 圓心的 x 座標 = 0
-          .attr('y1', () => 0) // y1 起點 = 圓心的 y 座標 = 0
-          .attr('x2', (_, i) => getCircleCenterX(i)) // x2 終點 = interactions 的 x 座標
-          .attr('y2', (_, i) => getCircleCenterY(i)); // y2 終點 = interactions 的 y 座標
+          .attr('x1', (_, i) => getStartX(i)) // 起點的 x 座標
+          .attr('y1', (_, i) => getStartY(i)) // 起點的 y 座標
+          .attr('x2', (_, i) => getCircleCenterX(i)) // 終點的 x 座標
+          .attr('y2', (_, i) => getCircleCenterY(i)); // 終點的 y 座標
+        // .attr('x1', d => d.source.x)
+        // .attr('y1', d => d.source.y)
+        // .attr('x2', d => d.target.x)
+        // .attr('y2', d => d.target.y);
 
         node.attr('cx', (_, i) => getCircleCenterX(i)).attr('cy', (_, i) => getCircleCenterY(i));
+        //node.attr('cx', d => d.x).attr('cy', d => d.y);
 
         text.attr('x', (_, i) => getCircleCenterX(i)).attr('y', (_, i) => getCircleCenterY(i) + 20);
+        //text.attr('x', d => d.x).attr('y', d => d.y + 20);
       });
-
+    console.log('graphData', graphData);
     return () => {
       simulation.stop();
     };
-  }, [targetAddress, isLoading, selectedItems]);
+  }, [graphData, selectedItems]);
 
-  const isShowGraph =
-    targetAddress && !isLoading ? (
-      <div id="container" ref={mapRef} className="h-600px w-1000px"></div>
-    ) : null;
+  const isShowGraph = targetAddress ? (
+    <div id="container" ref={mapRef} className="h-600px w-1000px"></div>
+  ) : null;
 
   return <>{isShowGraph}</>;
 };
