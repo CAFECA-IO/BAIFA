2c7837310c5f88ba664b66983b8b4b30a6fd330f
Luphia Chang <luphia@cafeca.io>
2024-10-20 18:07:16 +0800

    fix singleton mechanism

--- src/lib/utils/prisma.ts
+++ src/lib/utils/prisma.ts
@@ -8,8 +8,7 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+globalThis.prismaGlobal = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = globalThis.prismaGlobal;
 
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
