8d1db408a80f2ff3150d4e9cdd35232a685a85d3
julian <julian890701@gmail.com>
2026-01-19 18:46:50 +0800

    jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ const config = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.next/', '<rootDir>/test/'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.next/'],
 };
 
 module.exports = config;
