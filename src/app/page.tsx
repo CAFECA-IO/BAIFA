18701c577b37e58a93caff909f1665266ef2a3ac
julian <julian890701@gmail.com>
2026-01-21 11:28:50 +0800

    test eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import Navbar from '../components/common/navbar';
 
 export default function Home() {
-  console.log('Hello World');
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Navbar />
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <Image
           className="dark:invert"
